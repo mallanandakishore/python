5b57bcc0ae5ae6f5563003ceed7a7eb669350edc
mallanandakishore <73982107+mallanandakishore@users.noreply.github.com>
2020-11-11 10:30:01 +0530

    Add files via upload

---
+++ DAY-2/sample2.py
@@ -0,0 +1,13 @@
+print('hi')
+
+num1=80
+num2=30
+print('before swapping:',num1,num2)
+# temp=num2
+# num2=num1
+# num1=temp
+'''num1=num1+num2
+num2=num1-num2
+num1=num1-num2'''
+num1,num2=num2,num1
+print("after swapping:",num1,num2)
